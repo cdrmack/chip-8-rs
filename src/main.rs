58da5075fe22f3af50e8be376a842af15784cef4
cdrmack <git.pynvt@passmail.net>
2024-08-05 15:39:40 +0200

    add fetch method

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,45 @@
 use raylib::prelude::*;
 
-const WIDTH: i32 = 64;
-const HEIGHT: i32 = 32;
-const MULTIPLAYER: i32 = 10;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+const RAM: usize = 4096;
+const VRAM: usize = WIDTH * HEIGHT;
 
 struct Chip8 {
-    memory: [i8; 4096],
-    data_registers: [i8; 16],
-    index_register: i16,
-    stack: [i16; 16],
-    display: [bool; 2048],
-    keyboard: [i8; 16],
+    ram: [u8; RAM],
+    vram: [bool; VRAM],
+    pc: usize,
 }
 
 impl Chip8 {
     fn new() -> Self {
         Chip8 {
-            memory: [0; 4096],
-            data_registers: [0; 16],
-            index_register: 0,
-            stack: [0; 16],
-            display: [false; 2048],
-            keyboard: [0; 16],
+            ram: [0; RAM],
+            vram: [false; VRAM],
+            pc: 0x200,
         }
+    }
+
+    fn fetch(&self) -> u16 {
+        if self.pc < 0x200 {
+            panic!("trying to access reserved memory");
+        }
+
+        let first: u16 = (self.ram[self.pc] as u16) << 8;
+        let second: u16 = self.ram[self.pc + 1] as u16;
+        first | second
+    }
+
+    fn tick(&mut self) {
+        let _opcode = self.fetch();
+        // TODO: decode
+        // TODO: execute
     }
 }
 
 fn main() {
     let (mut rl, thread) = raylib::init()
-        .size(WIDTH * MULTIPLAYER, HEIGHT * MULTIPLAYER)
+        .size(WIDTH as i32 * 20, HEIGHT as i32 * 20)
         .title("CHIP-8")
         .build();
 
@@ -37,5 +48,27 @@ fn main() {
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fetch() {
+        let mut chip = Chip8::new();
+        chip.ram[chip.pc] = 0xA2;
+        chip.ram[chip.pc + 1] = 0xF0;
+
+        assert_eq!(0xA2F0, chip.fetch());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_fetch_reserved() {
+        let mut chip = Chip8::new();
+        chip.pc = 0x1FF;
+        chip.fetch();
     }
 }
