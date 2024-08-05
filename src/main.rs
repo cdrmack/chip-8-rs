9a3d4806a4f92940da48fcbdd1245752b6797bcf
cdrmack <git.pynvt@passmail.net>
2024-08-05 09:42:03 +0200

    add initial chip8 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct chip8 {
+    memory: [i8; 4096],
+    data_registers: [i8; 16],
+    index_register: i16,
+    stack: [i16; 128],
+    display: [bool; 2048],
+    keyboard: [i8; 16],
+}
+
+impl chip8 {
+    fn tick(&mut self) {
+        // TODO: fetch, decode, execute
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
