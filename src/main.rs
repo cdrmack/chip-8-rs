9677848db2fde3a508d6722e4e201f39fd58ae89
cdrmack <git.pynvt@passmail.net>
2024-08-05 12:08:51 +0200

    add Chip8 ctor

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,22 @@ struct Chip8 {
     memory: [i8; 4096],
     data_registers: [i8; 16],
     index_register: i16,
-    stack: [i16; 128],
+    stack: [i16; 16],
     display: [bool; 2048],
     keyboard: [i8; 16],
+}
+
+impl Chip8 {
+    fn new() -> Self {
+        Chip8 {
+            memory: [0; 4096],
+            data_registers: [0; 16],
+            index_register: 0,
+            stack: [0; 16],
+            display: [false; 2048],
+            keyboard: [0; 16],
+        }
+    }
 }
 
 fn main() {
@@ -18,6 +31,8 @@ fn main() {
         .size(WIDTH * MULTIPLAYER, HEIGHT * MULTIPLAYER)
         .title("CHIP-8")
         .build();
+
+    let _chip = Chip8::new();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
