7d9b70cdccde7278168a21755baff026df1b7bf1
cdrmack <git.pynvt@passmail.net>
2024-08-09 09:21:23 +0200

    handle opcodes 5XY0 and 9XY0

--- src/chip8.rs
+++ src/chip8.rs
@@ -110,11 +110,11 @@ impl Chip8 {
                     self.pc += 1;
                 }
             }
-            // (0x5, x, y, _) => {
-            //     if self.registers[x as usize] == self.registers[y as usize] {
-            //         self.pc += 1;
-            //     }
-            // }
+            (0x5, x, y, 0) => {
+                if self.registers[x as usize] == self.registers[y as usize] {
+                    self.pc += 1;
+                }
+            }
             (0x6, x, _, _) => {
                 let value: u8 = (opcode & 0x00FF) as u8;
                 self.registers[x as usize] = value;
@@ -123,11 +123,11 @@ impl Chip8 {
                 let value: u8 = (opcode & 0x00FF) as u8;
                 self.registers[x as usize] += value;
             }
-            // (0x9, x, y, _) => {
-            //     if self.registers[x as usize] != self.registers[y as usize] {
-            //         self.pc += 1;
-            //     }
-            // }
+            (0x9, x, y, 0) => {
+                if self.registers[x as usize] != self.registers[y as usize] {
+                    self.pc += 1;
+                }
+            }
             (0xA, _, _, _) => {
                 self.i = opcode & 0x0FFF;
             }
@@ -441,14 +441,51 @@ mod tests {
         assert_eq!(0x200, chip.pc);
     }
 
-    // #[test]
-    // fn test_5xy0_should_not_skip_instruction() {
-    //     let mut chip = Chip8::new();
-    //     chip.registers[0xA] = 0x41;
-    //     assert_eq!(0x200, chip.pc);
+    #[test]
+    fn test_5xy0_should_skip_instruction() {
+        let mut chip = Chip8::new();
+        chip.registers[0xA] = 0x41;
+        chip.registers[0xB] = 0x41;
+        assert_eq!(0x200, chip.pc);
 
-    //     chip.decode(0x3A42);
+        chip.decode(0x5AB0);
 
-    //     assert_eq!(0x200, chip.pc);
-    // }
+        assert_eq!(0x201, chip.pc);
+    }
+
+    #[test]
+    fn test_5xy0_should_not_skip_instruction() {
+        let mut chip = Chip8::new();
+        chip.registers[0xA] = 0x41;
+        chip.registers[0xB] = 0x42;
+        assert_eq!(0x200, chip.pc);
+
+        chip.decode(0x5AB0);
+
+        assert_eq!(0x200, chip.pc);
+    }
+
+    #[test]
+    fn test_9xy0_should_skip_instruction() {
+        let mut chip = Chip8::new();
+        chip.registers[0xA] = 0x41;
+        chip.registers[0xB] = 0x42;
+        assert_eq!(0x200, chip.pc);
+
+        chip.decode(0x9AB0);
+
+        assert_eq!(0x201, chip.pc);
+    }
+
+    #[test]
+    fn test_9xy0_should_not_skip_instruction() {
+        let mut chip = Chip8::new();
+        chip.registers[0xA] = 0x41;
+        chip.registers[0xB] = 0x41;
+        assert_eq!(0x200, chip.pc);
+
+        chip.decode(0x9AB0);
+
+        assert_eq!(0x200, chip.pc);
+    }
 }
